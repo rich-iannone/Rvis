7ae26dfb597eab472057b2b523b164160f80872a
rich-iannone <riannone@me.com>
2014-05-03 00:08:25 -0700

    Add newlines to generated tag strings

--- Rvis_tag.R
+++ Rvis_tag.R
@@ -2,9 +2,9 @@ Rvis_tag <- function(tag, o_c = "o"){
   
   # Create an open or closed tag
   if (o_c == "o"){
-    formatted_tag <- paste("<", tag, ">", sep = '')
+    formatted_tag <- paste("<", tag, ">\n", sep = '')
   } else if (o_c == "c"){
-    formatted_tag <- paste("</", tag, ">", sep = '')
+    formatted_tag <- paste("</", tag, ">\n", sep = '')
   }
   
   return(formatted_tag)
