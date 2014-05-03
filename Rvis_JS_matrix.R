262b473a1010b6bd58e287d9ae2ce8e16d832f94
rich-iannone <riannone@me.com>
2014-05-03 00:55:59 -0700

    Calculate the expected number of matrix values

--- Rvis_JS_matrix.R
+++ Rvis_JS_matrix.R
@@ -1,5 +1,7 @@
 Rvis_JS_matrix <- function(rows, columns, values){
   
-
+  # Calculate the expected number of values based on the numbers of rows and columns
+  expected_values <- rows * columns
+  
   
 }
