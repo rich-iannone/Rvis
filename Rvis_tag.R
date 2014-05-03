9c1d58eae47625e613e95c87a6e0f683df3677d1
rich-iannone <riannone@me.com>
2014-05-02 23:57:15 -0700

    Create and return an open or closed tag

--- Rvis_tag.R
+++ Rvis_tag.R
@@ -1,5 +1,12 @@
 Rvis_tag <- function(tag, o_c = "o"){
   
-
+  # Create an open or closed tag
+  if (o_c == "o"){
+    formatted_tag <- paste("<", tag, ">", sep = '')
+  } else if (o_c == "c"){
+    formatted_tag <- paste("</", tag, ">", sep = '')
+  }
+  
+  return(formatted_tag)
   
 }
