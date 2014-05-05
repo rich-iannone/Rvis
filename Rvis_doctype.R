9ca28c83f8d6deb3e8932729410f1a401beb030b
rich-iannone <riannone@me.com>
2014-05-04 20:07:52 -0700

    Move .R files to /R folder

--- Rvis_doctype.R
+++
@@ -1,8 +0,0 @@
-Rvis_doctype <- function(){
-
-  # Create HTML doctype declaration and meta charset tag
-  doctype_meta <- "<!DOCTYPE html>\n<meta charset=\"utf-8\">\n"
-
-  return(doctype_meta)
-
-}
