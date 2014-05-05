9eef4c0b6c88a40e93814378f434698a98b66dae
rich-iannone <riannone@me.com>
2014-05-05 00:04:55 -0700

    Get a vector list of variable names

--- R/Rvis_JS_var.R
+++ R/Rvis_JS_var.R
@@ -4,5 +4,12 @@ Rvis_JS_var <- function(sets){
   # Get the number of variables to set
   number_of_vars <- length(sets) / 2
   
+  # Get a vector list of variable names
+  for (i in seq(from = 1, to = length(sets) - 1, by = 2)){
+    if (i == 1) var_names <- vector(mode = "character", length = 0)
+    var_name <- sets[i]
+    var_names <- c(var_names, var_name)
+    if (i == length(sets) - 1) rm(var_name)
+  }
   
 }
