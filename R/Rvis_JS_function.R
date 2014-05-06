442b687a9ff4f8cfe421d2b074a4ce526a645dd7
rich-iannone <riannone@me.com>
2014-05-06 15:03:36 -0700

    Add test data to script

--- R/Rvis_JS_function.R
+++ R/Rvis_JS_function.R
@@ -1,4 +1,13 @@
 Rvis_JS_function <- function(sets){
  
-
+  # test data
+  sets <- c("svg.append", "g",
+            ".attr", "class", "y axis",
+            ".call", "~yAxis",
+            ".append", "text",
+            ".attr", "transform", "rotate(-90)",
+            ".attr", "y", "~6",
+            ".attr", "dy", ".71em",
+            ".style", "text-anchor", "end",
+            ".text", "Price ($)")
 }
