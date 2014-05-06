f832b426fa7c95c960f395b111a2238bdb30c594
rich-iannone <riannone@me.com>
2014-05-06 15:04:07 -0700

    Add comment block containing expected output

--- R/Rvis_JS_function.R
+++ R/Rvis_JS_function.R
@@ -10,4 +10,17 @@ Rvis_JS_function <- function(sets){
             ".attr", "dy", ".71em",
             ".style", "text-anchor", "end",
             ".text", "Price ($)")
+  # Final formatted output
+  
+  #   svg.append("g")
+  #   .attr("class", "y axis")
+  #   .call(yAxis)
+  #   .append("text")
+  #   .attr("transform", "rotate(-90)")
+  #   .attr("y", 6)
+  #   .attr("dy", ".71em")
+  #   .style("text-anchor", "end")
+  #   .text("Price ($)");
+  
+  
 }
