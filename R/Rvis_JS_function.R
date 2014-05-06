d6e8d5a2b252a10f7e80af481db93516a097f8ac
rich-iannone <riannone@me.com>
2014-05-06 15:04:25 -0700

    Add function to generate JS function

--- R/Rvis_JS_function.R
+++ R/Rvis_JS_function.R
@@ -10,6 +10,14 @@ Rvis_JS_function <- function(sets){
             ".attr", "dy", ".71em",
             ".style", "text-anchor", "end",
             ".text", "Price ($)")
+  
+  for (i in 1:length(sets)){
+    functions.indices <- grep("^([\\.][^0-9])", sets, value = TRUE)
+    quoted.parameters <- grep("^([^\\.~].*)|^([\\.][0-9].*)", sets, value = TRUE)
+    grep("^([a-zA-Z]+[\\.][a-zA-Z]*)", sets, value = TRUE)
+  }
+  
+  
   # Final formatted output
   
   #   svg.append("g")
